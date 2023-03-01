91033f4290432b3c96904d1e6e8e8fff490ef8b5
Аленов Михаил <banan.bananev@gmail.com>
2023-03-01 19:49:04 +0300

    aboba

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,32 +1,63 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, SafeAreaView, Text, TextInput, View, } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, SafeAreaView, Switch, Text, TextInput, View, } from 'react-native';
 
 export default function Login() {
     const navigation = useNavigation()
+    const [saveLogin, setSaveLogin] = useState(false)
 
-    return <View style={{
-        display: "flex",
-        alignItems: "center",
-        height: "100%",
-        flexDirection: "column",
-        justifyContent:"center"
-    }}>
-        <View style={{
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-            justifyContent: "center",
-        }}>
-            <Text>Login</Text>
-            <TextInput/>
-            <Text>Password</Text>
-            <TextInput/>
-            <Button 
-                onPress={()=>{
+    return <View style={styles.containerCol}>
+        <Text>Login</Text>
+        <TextInput style={styles.text} />
+        <Text>Password</Text>
+        <TextInput style={styles.text} />
+        <View style={styles.containerRow}>
+            <Text>Save Password</Text>
 
+            <Switch
+                value={saveLogin}
+                onValueChange={() => {
+                    setSaveLogin(!saveLogin)
+                }}
+            />
+        </View>
+
+        <View style={styles.containerRow}>
+            <Button
+                onPress={() => {
+                    navigation.navigate("Main")
                 }}
                 title="Submit"
+            />
+            <Button
+                onPress={() => {
+                    navigation.navigate("Main")
+                }}
+                title="Register"
             />
         </View>
     </View>
 }
+
+
+
+const styles = StyleSheet.create({
+    containerRow: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    containerCol: {
+        display: "flex",
+        alignItems: "center",
+        height: "100%",
+        flexDirection: "column",
+        justifyContent: "center"
+    },
+    text: {
+        margin: 5,
+        borderWidth: 2,
+        borderColor: "black"
+    }
+});
